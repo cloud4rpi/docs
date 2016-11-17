d277d3b7e1443223fcfc607e68efa7e0dbcfc375
Sky3d <sky3ddd@gmail.com>
2016-11-17 09:54:19 +0300

    specify output dir variable

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
+OUTPUT="site"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
@@ -18,20 +19,20 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
+git clone $REPO OUTPUT
+cd OUTPUT
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf out/**/* || exit 0
+rm -rf OUTPUT/**/* || exit 0
 
 # Run our compile script
 make build
 
 
 # Now let's go have some fun with the cloned repo
-cd out
+cd OUTPUT
 git config user.name "Travis CI"
 git config user.email "cloud4rpi@gmail.com"
 
@@ -58,6 +59,6 @@ chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 
-cd out
+cd OUTPUT
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
